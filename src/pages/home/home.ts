06236007fb9399504c44982a7af57a2f54a60e94
German-orjuela <rmartinezalf@gmail.com>
2018-08-15 21:29:12 -0500

    Primer aplicacion IONIC

---
+++ src/pages/home/home.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+export class HomePage {
+
+  constructor(public NavCtrl: NavController, public AlertCtrl: AlertController ) { }
+
+  showAlert() {
+    const alert = this.AlertCtrl.create({
+      title: 'New Friend!',
+      subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+}
+  
